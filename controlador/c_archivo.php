4bdc84a1d7592b0a53d86cc56053ebdd9f478712
christopher cortes <ccortesj.01@gmail.com>
2018-01-24 22:17:35 -0300

    respaldo 24.01

--- controlador/c_archivo.php
+++ controlador/c_archivo.php
@@ -1,10 +1,36 @@
 <?php 
-$s = $_FILES['archivo']['size'];
-$n = $_FILES['archivo']['name'];
-$t = $_FILES['archivo']['type'];
-$tp = $_FILES['archivo']['tmp_name'];
-echo $s;
-echo "<br>".$n;
-echo "<br>".$t;
-echo "<br>".$tp;
+include"../modelo/m_archivo.php";
+
+$archivo =  Archivo::ningunDato();
+
+$tamanoArch = $_FILES['archivo']['size'];
+$nombreArch = $_FILES['archivo']['name'];
+$tipoArch = $_FILES['archivo']['type'];
+$tmpArch = $_FILES['archivo']['tmp_name'];
+$fechaSubida = date("d-m-Y");
+$usuario = "crais";
+$idArch = $archivo->generarId();
+$ruta = str_replace("\\","/",dirname(__DIR__)."/archivos/".$usuario."/".$nombreArch);
+
+echo $ruta;
+echo "<br>".$fechaSubida;
+echo "<br>".$tmpArch;
+echo "<br>".$tamanoArch;
+echo "<br>".$_FILES['archivo']['size'];
+
+if(file_exists($ruta)){
+	echo "el archivo ya existe en el servidor";
+	die;
+}
+/* 83886080 bytes = 10 megas*/
+if($tamanoArch <= 83886080){
+echo "<br>".$tmpArch;
+$archivo2 = new Archivo($idArch,$tmpArch,$nombreArch,$tamanoArch,$fechaSubida,$usuario,$ruta,$tipoArch);
+
+$archivo2->subirArchivo();
+$archivo->guardarArchivo($tmpArch,$ruta);
+}else{
+	echo "<br>excede el tamaño del archivo";
+}
+
 ?>
--- modelo/m_archivo.php
+++ modelo/m_archivo.php
@@ -1,1 +1,67 @@
-m_archivo.php
+<?php
+include "conexion.php";
+class Archivo{
+	public $idArchivo;
+	public $tmpArchivo;
+	public $nombreArchivo;
+	public $tamanoArchivo;
+	public $fechaSubida;
+	public $usuario;
+	public $rutaArchivo;
+	public $tipoArchivo;
+
+	function __construct($idDoc,$tmpDoc,$nomAr,$tamAr,$fecha,$usu,$ruta,$tipo){
+
+		$this->idArchivo = $idDoc;
+		$this->tmpArchivo = $tmpDoc;
+		$this->nombreArchivo = $nomAr;
+		$this->tamanoArchivo = $tamAr;
+		$this->fechaSubida = $fecha;
+		$this->usuario = $usu;
+		$this->rutaArchivo = $ruta;
+		$this->tipoArchivo = $tipo;
+	}
+
+	static function ningunDato(){
+		return new self("","","","","","","","");
+	}
+
+	function subirArchivo(){
+		echo "<br> funcion subir archivo";
+		echo "<br> fecha : " . $this->fechaSubida;
+	die;
+		$db = new Conexion();
+		$sql = "insert into archivo(id_doc,nombre,tipo,peso,fecha_subida,ruta)values('$this->idArchivo','$this->nombreArchivo','$this->tamanoArchivo','$this->fechaSubida','$this->usuario','$this->rutaArchivo')";
+		$db->query($sql) ? $m = "archivo subido" : $m = "problema subir archivo";
+		echo $m;
+		$sql = "insert into usuario_archivo(usuario,id_doc)values('$this->usuario','$this->idArchivo')";
+		$db->query($sql) ? $m="<br>insertado" : $m = "<br>no insertado";
+		echo $m;
+
+	}
+
+	function guardarArchivo($ar,$rt){
+		echo "<br>ar : " . $ar;
+		echo "<br>rt: " . $rt;
+		move_uploaded_file($ar, $rt);
+	}
+
+	function formatBytes($au, $precision = 2)
+	{
+    	$base = log($au, 1024);
+    	$suffixes = array('B', 'K', 'M', 'G', 'T');   
+
+    	return round(pow(1024, $base - floor($base)), $precision) .' '. $suffixes[floor($base)];
+	}
+
+	function generarId() {
+    $chr = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ|!#&?';
+    $id="";
+    $chrLng = strlen($chr);
+    for ($i = 0; $i < 50; $i++) {
+        $id .= $chr[rand(0, $chrLng - 1)];
+    }
+    return $id;
+}
+}
+?>
